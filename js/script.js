6808704138d2932cf85804ef17ed2190a76b187b
Justa <filjust@gmail.com>
2017-04-09 13:48:02 +0200

    aktualizacja

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
+$(function() {
+	smoothScroll(500);
+	goToTop();
+});
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
 (function($) {
 
 	$('.swipebox').swipebox();
 
 } )(jQuery);
 
-(function($) {
-  var controller = new slidebars();
-  controller.init();
-  
-  $('#menu').click(function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-
-    controller.toggle('mobile-menu');
-  });
-})(jQuery);
+function goToTop () {
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scroll').fadeIn();
+		} else {
+			$('.scroll').fadeOut();
+		}
+	});
+	$('.scroll').click(function(){
+		$('html, body').animate({scrollTop : 0},500);
+		return false;
+	});
+	
+}
